866c98b1c7952a8df66b6d8a868a8ea1839137b7
Patrick Gac <pgac85@gmail.com>
2014-11-19 15:15:29 -0600

    dbURL Fix

--- mongodb.js
+++ mongodb.js
@@ -7,7 +7,7 @@ exports.init = function (onCreate) {
 	dbPass = process.env.OPENSHIFT_MONGODB_DB_PASSWORD;
 
 	if (process.env.NODE_ENV === "production") {
-		dbUrl = OPENSHIFT_MONGODB_DB_URL;
+		dbUrl = process.env.OPENSHIFT_MONGODB_DB_URL;
 	} else {
 		dbUrl = "mongodb://127.0.0.1:27017/shlodge124";
 	}
